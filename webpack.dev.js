25db5320d1397595344eee734fe823615b14e591
caetano <joaoluisfcaetano@gmail.com>
2020-11-25 22:33:44 -0300

    Update dev dependencies and create the webpack config files for production and development environments

---
+++ webpack.dev.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin ({
+            template: './src/client/pages/index.html',
+            filename: './index.html'
+        })
+    ]
+}
